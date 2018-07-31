9ddcd62e8be20b3246a0e7fbd528ac06fd4ce84a
Charlie Guan <charliegdev@gmail.com>
2018-07-31 15:24:04 +0800

    Display titles from the list items.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import list from "./components/local-data";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list
+    };
+  }
   render() {
     return (
       <div className="App">
         <h1 className="text-center">Hacker News Redux</h1>
+        <ul className="list-group">
+          {this.state.list.map(item => <li className="list-group-item" key={item.objectID}>{item.title}</li>)}
+        </ul>
       </div>
     );
   }
---
+++ src/components/local-data.js
@@ -0,0 +1,20 @@
+const list = [
+  {
+    title: "React",
+    url: "https://facebook.gitub.io/react",
+    author: "Jordon Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0
+  }, 
+  {
+    title: "Redux",
+    url: "https://facebook.github.io/redux",
+    author: "Test Author",
+    num_comments: 5,
+    points: 3,
+    objectID: 1
+  }
+];
+
+export default list;
