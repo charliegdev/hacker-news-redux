5df1fe466a03fe83ab60b1712e236bbd3dc94e91
Charlie Guan <charliegdev@gmail.com>
2018-08-02 16:06:54 +0800

    Implement actual search functionality.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import list from "./components/local-data";
 import NewsList from "./components/NewsList";
 import SearchField from "./components/SearchField";
@@ -7,14 +7,37 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      list
+      list, // actual list. Affected by deletion.
+      displayedList: list // displayed list. Affected by searching and deletion.
     };
     this.onDelete = this.onDelete.bind(this);
+    this.onSearchComplete = this.onSearchComplete.bind(this);
+    this.onSearchUpdate = this.onSearchUpdate.bind(this);
   }
 
   onDelete(objectID) {
-    const newList = this.state.list.filter(item => item.objectID !== objectID);
-    this.setState({list: newList});
+    const { list, displayedList } = this.state;
+    const matchObjectID = item => item.objectID !== objectID;
+
+    const remainingList = list.filter(matchObjectID);
+    const remainingDisplayedList = displayedList.filter(matchObjectID);
+
+    this.setState({ 
+      list: remainingList,
+      displayedList: remainingDisplayedList
+    });
+  }
+
+  onSearchUpdate(newSearchValue) {
+    const filteredList = this.state.list.filter(item =>
+      item.title.toLowerCase().includes(newSearchValue.toLowerCase())
+    );
+    this.setState({ displayedList: filteredList });
+  }
+
+  onSearchComplete(event) {
+    // currently we don't need to do anything.
+    event.preventDefault();
   }
 
   render() {
@@ -23,8 +46,11 @@ class App extends Component {
         <br />
         <h1 className="text-center">Hacker News Redux</h1>
         <br />
-        <SearchField />
-        <NewsList list={this.state.list} deleteFunc={this.onDelete} />
+        <SearchField
+          onSubmitFunc={this.onSearchComplete}
+          onChangeFunc={this.onSearchUpdate}
+        />
+        <NewsList list={this.state.displayedList} deleteFunc={this.onDelete} />
       </div>
     );
   }
--- src/components/SearchField.js
+++ src/components/SearchField.js
@@ -1,43 +1,65 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 
-const SearchField = props => {
-  const { onChangeFunc } = props;
-  return (
-    <div>
-      <p>
-        <button
-          className="btn btn-primary"
-          type="button"
-          data-toggle="collapse"
-          data-target="#collapseExample"
-          aria-expanded="false"
-          aria-controls="collapseExample"
-        >
-          Search
-        </button>
-      </p>
-      <div className="collapse" id="collapseExample">
-        <div className="card card-body">
-          <form>
-            <div className="form-group">
-              <label htmlFor="searchField">Search a Title</label>
-              <input
-                type="text"
-                className="form-control"
-                id="searchField"
-                placeholder="Enter part of a title"
-              />
-              <small id="searchFieldHelp" className="form-text text-muted">
-                Searching for locally cached news
-              </small>
-            </div>
-          </form>
+class SearchField extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { searchValue: "" };
+
+    this.onSearchFieldChange = this.onSearchFieldChange.bind(this);
+  }
+
+  onSearchFieldChange(event) {
+    const newSearchValue = event.target.value;
+    this.setState({ searchValue: newSearchValue });
+    this.props.onChangeFunc(newSearchValue);
+  }
+
+  render() {
+    const { onSubmitFunc } = this.props;
+    return (
+      <div>
+        <p>
+          <button
+            className="btn btn-primary"
+            type="button"
+            data-toggle="collapse"
+            data-target="#collapseExample"
+            aria-expanded="false"
+            aria-controls="collapseExample"
+          >
+            Search
+          </button>
+        </p>
+        <div className="collapse" id="collapseExample">
+          <div className="card card-body">
+            <form onSubmit={onSubmitFunc} onChange={this.onSearchFieldChange}>
+              <div className="form-group">
+                <label htmlFor="searchField">Search a Title</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="searchField"
+                  placeholder="Enter part of a title"
+                  value={this.state.searchValue}
+                />
+                <small id="searchFieldHelp" className="form-text text-muted">
+                  Searching for locally cached news
+                </small>
+              </div>
+            </form>
+          </div>
         </div>
+        <br />
       </div>
-      <br />
-    </div>
-  );
+    );
+  }
+}
+
+SearchField.propTypes = {
+  onChangeFunc: PropTypes.func.isRequired,
+  onSubmitFunc: PropTypes.func.isRequired
 };
 
 export default SearchField;
