97b3a9200da92bb11d6d6cc62cd58ff4f74c8010
Charlie Guan <charliegdev@gmail.com>
2018-08-12 14:10:34 +0800

    Use ref for the SearchField component to enable focus.

--- src/components/SearchField/SearchField.js
+++ src/components/SearchField/SearchField.js
@@ -1,42 +1,49 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 
-const SearchField = ({ onSubmitFunc, onChangeFunc, searchValue, children = "Search" }) =>
-  <div>
-    <p>
-      <button
-        className="btn btn-primary"
-        type="button"
-        data-toggle="collapse"
-        data-target="#collapseExample"
-        aria-expanded="false"
-        aria-controls="collapseExample"
-      >
-        {children}
-      </button>
-    </p>
-    <div className="collapse" id="collapseExample">
-      <div className="card card-body">
-        <form onSubmit={onSubmitFunc}>
-          <div className="form-group">
-            <label htmlFor="searchField">Search a Title</label>
-            <input
-              type="text"
-              className="form-control"
-              id="searchField"
-              placeholder="Enter part of a title"
-              value={searchValue}
-              onChange={event => onChangeFunc(event.target.value)}
-            />
-            <small id="searchFieldHelp" className="form-text text-muted">
-              Searching for remote news
-            </small>
-          </div>
-        </form>
+class SearchField extends Component {
+  componentDidMount() {
+    if (this.input) this.input.focus();
+  }
+
+  render() {
+    const { onSubmitFunc, onChangeFunc, searchValue, children = "Search" } = this.props;
+    return (
+      <div>
+        <div className="card card-body">
+          <form onSubmit={onSubmitFunc}>
+            <div className="form-group">
+              <label htmlFor="searchField">Search a Title</label>
+              <input
+                type="text"
+                className="form-control"
+                id="searchField"
+                placeholder="Enter part of a title"
+                value={searchValue}
+                onChange={event => onChangeFunc(event.target.value)}
+                ref={node => this.input = node}
+              />
+              <small id="searchFieldHelp" className="form-text text-muted">
+                Searching for remote news
+              </small>
+            </div>
+            <button
+              className="btn btn-primary"
+              type="submit"
+              data-toggle="collapse"
+              data-target="#collapseExample"
+              aria-expanded="false"
+              aria-controls="collapseExample"
+            >
+              {children}
+            </button>
+          </form>
+        </div>
+        <br />
       </div>
-    </div>
-    <br />
-  </div>
+    );
+  }
+} 
 
 SearchField.propTypes = {
   onChangeFunc: PropTypes.func.isRequired,
