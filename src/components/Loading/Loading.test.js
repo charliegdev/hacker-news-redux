c40ccc1e0903993d564ca63e8eb6722abfeb5437
Charlie Guan <charliegdev@gmail.com>
2018-08-12 14:22:00 +0800

    Add a new Loading component and its tests.

--- src/components/Loading/Loading.test.js
+++ src/components/Loading/Loading.test.js
@@ -1,37 +1,31 @@
 import React from "React";
 import ReactDOM from "react-dom";
 import renderer from "react-test-renderer";
-import Enzyme, { render } from "enzyme";
+import Enzyme from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
-import NewsList from "./NewsList";
+import Loading from "./Loading";
 
 Enzyme.configure({ adapter: new Adapter() });
 
 describe("NewsList", () => {
-  const mockFunc = () => "mockFunc";
-  const props = {
-    list: [
-      { title: "1", author: "1", num_comments: 1, points: 2, objectID: "abc" },
-      { title: "2", author: "2", num_comments: 2, points: 3, objectID: "xyz" },
-    ],
-    deleteFunc: mockFunc
-  }
 
-  const newsList = <NewsList {...props} />;
+  const loading = <Loading />;
   it("renders without crashing", () => {
     const div = document.createElement("div");
-    ReactDOM.render(newsList, div);
+    ReactDOM.render(loading, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 
   test("has a valid snapshot", () => {
-    const component = renderer.create(newsList);
+    const component = renderer.create(loading);
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
 
+  /*
   it("shows 2 items", () => {
-    const element = render(newsList); // can't use shallow because I need child components to be rendered
+    const element = shallow(loading); // can't use shallow because I need child components to be rendered
     expect(element.find(".btn-outline-danger")).toHaveLength(2);
   });
+  */
 });
